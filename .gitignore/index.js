baac39cbc5c99b04dabe27a7b183c7f03f118473
Elflick <39348290+Elflick@users.noreply.github.com>
2018-05-18 18:50:38 +0200

    Update index.js

--- .gitignore/index.js
+++ .gitignore/index.js
@@ -4,7 +4,7 @@ const bot = new Discord.Client();
 var prefix = ("!")
 
 bot.on('ready', function() {
-    bot.user.setGame("Préfix: ! / !help");
+    bot.user.setGame("dormir");
     console.log("Connecté");
 });
 
@@ -12,13 +12,43 @@ bot.login("NDQ1OTgyMDkwMzI1MTMxMjY3.Dd9p3A.XK03ZFMunuNvVUFz-EFXM0s8L34");
 
 
 bot.on('message', message => {
-    if (message.content === prefix + "help") {
+    if (message.content === prefix + "help"){
         message.channel.sendMessage("**Liste des commandes:** \n !help");
     }
 
-    if (message.content === "!infos") {
+    if (message.content === "!info"){
     message.reply("**Créateur:** Arès \n**Discord:** https://discord.gg/g4CBXm")
     console.log("Commande !infos effectué");
     }
 
 });
+
+
+if(cmd === `${prefix}kick`){
+
+    //!kick @daeshan askin for it
+
+    let kUser = message.guild.member(message.mentions.users.first() || message.guild.members.get(args[0]));
+    if(!kUser) return message.channel.send("Utilisateur Introuvable!");
+    let kReason = args.join(" ").slice(22);
+    if(!message.member.hasPermission("MANAGE_MESSAGES")) return message.channel.send("Vous ne pouvez pas le faire.")
+    if(kUser.hasPermission("MANAGE_MESSAGES")) return message.channel.send("Cette personne ne peux pas être kick.")
+
+    let kickEmbed = new Discord.RichEmbed()
+    .setDescription("~Kick~")
+    .setColor("#00ba94")
+    .addField("Utilisateur kick", `${kUser} with ID ${kUser.id}`)
+    .addField("Kick par", `<@${message.author.id}> with ID ${message.author.id}`)
+    .addField("Tiime", message.createdAt)
+    .addField("Raison", kReason);
+
+    let kickChannel = message.guild.channel.find(`name`, "👋bonjour-aurevoir");
+    if(!kickChannel) return message.channel.send("Channel introuvable.")
+    
+    message.guilf.member(kMember).kick(kReason);
+    kickChannel.send(kickEmbed);
+
+
+    return;
+}
+
