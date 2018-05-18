a9c03e230828db868da4c796a6aed55d6ab01306
Elflick <39348290+Elflick@users.noreply.github.com>
2018-05-18 11:04:02 +0200

    Create index.js

---
+++ .gitignore/index.js
@@ -0,0 +1,24 @@
+const Discord = require('discord.js');
+const bot = new Discord.Client();
+
+var prefix = ("!")
+
+bot.on('ready', function() {
+    bot.user.setGame("Préfix: ! / !help");
+    console.log("Connecté");
+});
+
+bot.login("NDQ1OTgyMDkwMzI1MTMxMjY3.Dd9p3A.XK03ZFMunuNvVUFz-EFXM0s8L34");
+
+
+bot.on('message', message => {
+    if (message.content === prefix + "help") {
+        message.channel.sendMessage("**Liste des commandes:** \n !help");
+    }
+
+    if (message.content === "!infos") {
+    message.reply("**Créateur:** Arès \n**Discord:** https://discord.gg/g4CBXm"), 'message.receveid', function(bot,message) {
+    console.log("Commande !infos effectué");
+    }
+
+});
